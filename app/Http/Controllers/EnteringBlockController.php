403a56aeab60762d8c6890b2e1d3ed1ae3f40d38
Reza Pishva <rpishva@gmail.com>
2024-01-09 13:01:33 +0330

    added to sources

--- app/Http/Controllers/EnteringBlockController.php
+++ app/Http/Controllers/EnteringBlockController.php
@@ -1,11 +1,18 @@
 <?php
 namespace App\Http\Controllers;
+use App\CalendarHelper;
+use Carbon\Carbon;
+
 use App\Models\EnteringBlock;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 class EnteringBlockController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
     public function blocks(){
         $blocks = EnteringBlock::all();
         return response()->json(['success'=>'hi','result'=>$blocks]);
@@ -52,6 +59,21 @@ class EnteringBlockController extends Controller
         
     }
     public function set_block1($id){
+
+        // $user = auth()->user()->id;
+        // $f_name=auth()->user()->f_name;
+        // $l_name=auth()->user()->l_name;
+        // $full_name=$f_name.' '.$l_name;
+        // $g_y = Carbon::now()->year;
+        // $g_m = Carbon::now()->month;
+        // $g_d = Carbon::now()->day;
+        // $Calendar=new CalendarHelper();
+        // $date_shamsi_array=$Calendar->gregorian_to_jalali($g_y, $g_m, $g_d);
+        // $date_shamsi=$date_shamsi_array[0].'/'.$date_shamsi_array[1].'/'.$date_shamsi_array[2];
+        // $mytime=Carbon::now();
+        // $values = array('national_code'=> 0,'date_shamsi'=>$date_shamsi,'time_set' => $mytime,'description' =>$reason,'userblock'=>$full_name,'block'=>"این فرد بلاک شد");
+        // DB::table('workflowblocks')->insert($values);
+
         EnteringBlock::where('id_b',$id)->update(['isBlocked'=>1]);
         return response()->json(['success'=>'hi','result'=>$id]);
     }
